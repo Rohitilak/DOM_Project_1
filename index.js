00f1e5f64a16120a5dec7221c5868928b04756f6
Rohitilak <rohittilak086@gmail.com>
2024-01-09 23:19:49 +0530

    dom project

---
+++ index.js
@@ -0,0 +1,22 @@
+const div = document.getElementById("root");
+
+div.style.backgroundColor = "yellow";
+div.style.margin = "20px";
+
+div.style.padding = "10px";
+
+div.style.fontSize = "18px";
+
+div.style.fontWeight = "bold";
+
+div.style.height = "200px";
+
+div.style.width = "300px";
+
+
+
+const h1 = document.querySelector("h1");
+h1.style.fontSize = "17px";
+h1.style.color = "blue";
+
+
